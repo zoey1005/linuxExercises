0c8b0300ba947d5d1d78a5b489f25fcc9178d2ed
zoey1005 <100222843+zoey1005@users.noreply.github.com>
2022-03-11 20:00:40 -0600

    Update school.sh

--- school.sh
+++ school.sh
@@ -1,2 +1,3 @@
-SUM is 24154170100
-AVERAGE is 346987.8338 
+#!/bin/bash
+
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOL" | cut -d, -f 7 | {sum=0; num=0; while read x; do sum=$(($sum+$x)); num=$(($num+1)); done; echo $sum/$num;}
