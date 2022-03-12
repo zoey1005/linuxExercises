b07e6bcf9c4d06a66f00ea44f6123147bb21651e
zoey1005 <100222843+zoey1005@users.noreply.github.com>
2022-03-11 18:21:34 -0600

    Update digits.sh

--- digits.sh
+++ digits.sh
@@ -1,2 +1,13 @@
+#!/bin/bh                                                                       
+total=0
+for i in range(1000,2001):
+    r=0
+    for j in str(i):
+        if int(j) in [0,1]:
+            r+=1
+
+        if r==4:
+            total +=i
+print("total sum ={}".format(total))
 
 TOTAL SUM = 8444
