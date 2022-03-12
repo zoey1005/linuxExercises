90c4f43245119300a7f1e2c9e87659bbfaec547a
zoey1005 <100222843+zoey1005@users.noreply.github.com>
2022-03-11 19:51:05 -0600

    Update five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,2 +1,13 @@
-cd Desktop/linuxExercises
-mkdir -p five/{dir1,dir2,dir3,dir4,dir5}
+#!/bin/bash
+
+mkdir five
+for i in {1..5};do
+    mkdir five/dir$i;
+    for num in {1..4};do
+        >five/dir$i/file$num
+        
+        for ((line=1;line<=$num;line++)); do
+            echo $num >> five/dir$i/file$num
+        done
+    done
+done
